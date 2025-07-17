316a26c3bf630424e7de1b8673ec0b264100da0a
UNGBI78 <mybluewings78@gmail.com>
2025-07-17 18:44:18 +0900

    ✨ 선택 가능한 번호 추가
    입금(1), 출금(2), 영수증 보기(3) 선택 가능한 조건 작

--- atm.py
+++ atm.py
@@ -9,4 +9,14 @@ while True:
     if num == '4':
         break
 
+    if num == '1':
+        pass 
+
+    if num == '2':
+        pass
+
+    if num == '3':
+        pass
+    
+
 print(f'시스템이 종료되었습니다. 현재 잔액은 {balance}원 입니다.')
